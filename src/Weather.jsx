6ce98625211a57565c7e4bca1d21237a0836aedc
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-06 17:30:19 +0530

    created  new component

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -8,15 +8,28 @@ import sunIcon from "./assets/sunny.png"
 import { FaSearch } from "react-icons/fa";
 
 
-const WeatherDetails = ({ icon , temp , city , country }) => {
+
+
+//  create a new component and passing props 
+const WeatherDetails = ({ icon, temp, city, country ,lat,long }) => {
     return (
         <>
             <div className="image">
-                <img src={icon} style={{ width: 250, height: 250,  padding:10}} alt="image" />
+                <img className="sep-img" src={icon} style={{ width: 160, height: 160, padding: 10 }} alt="image" />
             </div>
-            <div>{temp}</div>
-            <div>{city}</div>
-            <div>{country}</div>
+            <div className="temp">{temp}°C</div>
+            <div className="city">{city}</div>
+            <div className="country">{country}</div>
+            <div className="cord">
+                <div className="latitude">
+                    <span>latitude</span>
+                    <span>{lat}</span>
+                </div>
+                <div className="longitude">
+                    <span>longitude</span>
+                    <span>{long}</span>
+                </div>
+            </div>
         </>
     )
 
@@ -25,11 +38,12 @@ const WeatherDetails = ({ icon , temp , city , country }) => {
 
 const Weather = () => {
     // create state variables
-    const [icon, setIcon] = useState( sunIcon )
-    const [temp, setTemp] = useState( 0 )
-    const [city, setCity] = useState( "ERODE" )
-    const [country, setCountry] = useState( "IND" )
-    
+    const [icon, setIcon] = useState(sunIcon)
+    const [temp, setTemp] = useState(0)
+    const [city, setCity] = useState("ERODE")
+    const [country, setCountry] = useState("IND")
+    const [lat,setLat]=useState(0)
+    const [long,setLong]=useState(0)
     return (<>
         {/* <h1>Weather application</h1> */}
         <div className="container">
@@ -37,8 +51,10 @@ const Weather = () => {
                 <input className="input-search" type="text" placeholder="Search City" />
                 <FaSearch className="search-icon" />
             </div>
-            <WeatherDetails icon={icon} temp={temp} city={city} country={country} />
+            {/* passing a created component and sent props here  */}
+            <WeatherDetails icon={icon} temp={temp} city={city} country={country} lat={lat} long={long} />
         </div>
+
     </>);
 }
 
