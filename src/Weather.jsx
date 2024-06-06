0d1541079647efa511f5913551ecf027b368471b
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-06 15:15:14 +0530

    created styles

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,9 +1,23 @@
+// import freezeIcon from "./assets/black-freeze.jpg"
+// import snowIcon from "./assets/black-snow.jpg"
+// import cloudIcon from "./assets/cloudy.png"
+// import rainIcon from "./assets/rainy.jpg"
+// import sunIcon from "./assets/sunny.jpg"
+// import thunderIcon from "./assets/thunder.jpg"
+import { FaSearch } from "react-icons/fa";
+
+
+
+
 
 const Weather = () => {
     return (<>
-<h1>WEATHER APP</h1>
-<div>
-    
+{/* <h1>Weather application</h1> */}
+<div className="container">
+    <div className="search-container">
+        <input className="input-search" type="text" placeholder="Search City"/>
+        <FaSearch className="search-icon" />
+    </div>
 </div>
     </>);
 }
