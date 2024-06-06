ad1c233125133fde39660f9ce96adda2582a61b7
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-06 16:48:07 +0530

    state variables created

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -2,23 +2,41 @@
 // import snowIcon from "./assets/black-snow.jpg"
 // import cloudIcon from "./assets/cloudy.png"
 // import rainIcon from "./assets/rainy.jpg"
-// import sunIcon from "./assets/sunny.jpg"
+import { useState } from "react";
+import sunIcon from "./assets/sunny.png"
 // import thunderIcon from "./assets/thunder.jpg"
 import { FaSearch } from "react-icons/fa";
 
 
+const WeatherDetails = ({ icon }) => {
 
+    return (
+        <>
+            <div className="image">
+                <img src={icon} style={{ width: 250, height: 250,  padding:10}} alt="image" />
+            </div>
+        </>
+    )
+
+}
 
 
 const Weather = () => {
+    // create state variables
+    const [icon, setIcon] = useState( sunIcon )
+    const [temp, setTemp] = useState( 0 )
+    const [city, setCity] = useState( "ERODE" )
+    const [country, setCountry] = useState( "IND" )
+    
     return (<>
-{/* <h1>Weather application</h1> */}
-<div className="container">
-    <div className="search-container">
-        <input className="input-search" type="text" placeholder="Search City"/>
-        <FaSearch className="search-icon" />
-    </div>
-</div>
+        {/* <h1>Weather application</h1> */}
+        <div className="container">
+            <div className="search-container">
+                <input className="input-search" type="text" placeholder="Search City" />
+                <FaSearch className="search-icon" />
+            </div>
+            <WeatherDetails icon={icon} />
+        </div>
     </>);
 }
 
